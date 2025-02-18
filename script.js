918e3eb663fd4e9241aa8283514403a55ad64a2d
nairatch <nairatch@ymail.com>
2025-02-18 20:43:07 +0400

    Updated location messages

--- script.js
+++ script.js
@@ -8,64 +8,112 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Define location messages
     const locationMessages = {
-        "Eswatini/Mbabane": "This location is currently burnt for us!",
-        "South Africa/Cape Town": "This location is perfect, it could work for any athlete!",
-        "South Africa/Johannesburg": "Consider this location carefully, some applications are rejected.",
-        "Mauritius/Port Louis": "deatils will be added.",
-        "Zambia/Lusaka": "Processing times here are slightly longer than usual.",
-        "Zimbabwe/Harare": "Athletes have mixed results here, check requirements.",
-        "Nigeria/Lagos": "High rejection rates, proceed with caution.",
-        "Turkey/Istanbul": "info will be added",
-        "Poland/Warsaw": "info will be added",
-        "India/Mumbai": "info will be added",
-        "Sri Lanka/Colombo": "info will be added."
+        "Austria/Vienna": "Info will be added",
+        "Bahrain/Manama": "Info will be added",
+        "Belgium/Brussels": "Info will be added",
+        "Bosnia & Herzegovina/Sarajevo": "Info will be added",
+        "Croatia/Zagreb": "Info will be added",
+        "Cyprus/Nicosia": "Info will be added",
+        "Czech Republic/Prague": "Info will be added",
+        "Denmark/Copenhagen": "Info will be added",
+        "Estonia/Tallinn": "Info will be added",
+        "Eswatini/Mbabane": "Info will be added",
+        "Finland/Helsinki": "Info will be added",
+        "France/Paris": "Info will be added",
+        "Germany/Berlin": "Info will be added",
+        "Germany/Munich": "Info will be added",
+        "Germany/Frankfurt": "Info will be added",
+        "Greece/Athens": "Info will be added",
+        "Hungary/Budapest": "Info will be added",
+        "Iceland/Reykjavik": "Info will be added",
+        "India/Chennai": "Info will be added",
+        "India/New Delhi": "Info will be added",
+        "India/Kolkata": "Info will be added",
+        "India/Mumbai": "Info will be added",
+        "India/Hyderabad": "Info will be added",
+        "Italy/Rome": "Info will be added",
+        "Italy/Milan": "Info will be added",
+        "Japan/Tokyo": "Info will be added",
+        "Latvia/Riga": "Info will be added",
+        "Liberia/Monrovia": "Info will be added",
+        "Lithuania/Vilnius": "Info will be added",
+        "Luxembourg/Luxembourg": "Info will be added",
+        "Madagascar/Antananarivo": "Info will be added",
+        "Mauritius/Port Louis": "Info will be added",
+        "Nigeria/Lagos": "Info will be added",
+        "Nigeria/Abuja": "Info will be added",
+        "Norway/Oslo": "Info will be added",
+        "Poland/Warsaw": "Info will be added",
+        "Poland/Krakow": "Info will be added",
+        "Portugal/Lisbon": "Info will be added",
+        "Romania/Bucharest": "Info will be added",
+        "Senegal/Dakar": "Info will be added",
+        "Serbia/Belgrade": "Info will be added",
+        "South Africa/Cape Town": "Info will be added",
+        "South Africa/Durban": "Info will be added",
+        "South Africa/Johannesburg": "Info will be added",
+        "Sri Lanka/Colombo": "Info will be added",
+        "Sweden/Stockholm": "Info will be added",
+        "Switzerland/Bern": "Info will be added",
+        "The Netherlands/Amsterdam": "Info will be added",
+        "Togo/Lome": "Info will be added",
+        "Zimbabwe/Harare": "Info will be added"
     };
-// Define detailed button information for locations
-const locationDetails = {
-    "South Africa/Cape Town": {
-        "approval": "High",
-        "processing": "Usually 3-4 business days - then delivery to pick up station (1-2 additional days).",
-        "approved": `Nelly Jemeli, 
-                    Brenda Kibor, 
-                    Mercy Lomuria, 
-                    Millicent Kemboi, 
-                    Viola Jepleting, 
-                    Naomi Jepleting, 
-                    Faith Kipmaiyo, 
-                    Caren Kiplagat`,
-        "denied": "No one denied."
-    }
-};
     
-visaLocationSelect.addEventListener("change", function () {
-    const selectedLocation = visaLocationSelect.value;
 
-    if (selectedLocation) {
-        infoText.textContent = locationMessages[selectedLocation] || "Information for this location will be added soon.";
-        locationInfoDiv.classList.remove("hidden");
-        locationInfoDiv.dataset.location = selectedLocation; // Set the data-location attribute
-    } else {
-        locationInfoDiv.classList.add("hidden");
-    }
-});
+    // Define detailed button information for locations
+    const locationDetails = {
+        "South Africa/Cape Town": {
+            "waitingtime": "30 days (18.02.2025). <br> Please also check the <a href='https://travel.state.gov/content/travel/en/us-visas/visa-information-resources/global-visa-wait-times.html' target='_blank'>Global Visa Wait Times</a> page.",
+            "approval": "High",
+            "processing": "Usually 3-4 business days - then delivery to pick up station (1-2 additional days).",
+            "approved": `Nelly Jemeli, 
+                        Brenda Kibor, 
+                        Mercy Lomuria, 
+                        Millicent Kemboi, 
+                        Viola Jepleting, 
+                        Naomi Jepleting, 
+                        Faith Kipmaiyo, 
+                        Caren Kiplagat`,
+            "denied": "No one denied."
+        }
+    };
 
+    visaLocationSelect.addEventListener("change", function () {
+        const selectedLocation = visaLocationSelect.value;
+    
+        // Clear previously displayed information
+        const infoBoxes = document.querySelectorAll(".info-box");
+        infoBoxes.forEach(box => box.remove());
+    
+        if (selectedLocation) {
+            infoText.textContent = locationMessages[selectedLocation] || "Information for this location will be added soon.";
+            infoText.classList.add("message-box"); // Apply styling class
+            locationInfoDiv.classList.remove("hidden");
+            locationInfoDiv.dataset.location = selectedLocation;
+        } else {
+            locationInfoDiv.classList.add("hidden");
+        }
+    });
+    
 
     window.showMoreInfo = function (infoType) {
         const selectedLocation = locationInfoDiv.dataset.location;
         let moreInfo = {
+            "waitingtime": "Details will be added. <br> Please also check the <a href='https://travel.state.gov/content/travel/en/us-visas/visa-information-resources/global-visa-wait-times.html' target='_blank'>Global Visa Wait Times</a> page.",
             "approval": "Details will be added.",
             "questions": "Details will be added.",
             "processing": "Details will be added.",
             "approved": "Details will be added.",
             "denied": "Details will be added."
         };
-    
+
         // Check if the selected location has specific data
         let displayText = moreInfo[infoType] || "More details will be updated soon.";
         if (selectedLocation && locationDetails[selectedLocation] && locationDetails[selectedLocation][infoType]) {
             displayText = locationDetails[selectedLocation][infoType];
         }
-    
+
         // Check if an info box for this type already exists
         let existingInfoBox = document.querySelector(`.info-box[data-type="${infoType}"]`);
         if (!existingInfoBox) {
@@ -73,8 +121,10 @@ visaLocationSelect.addEventListener("change", function () {
             let infoBox = document.createElement("div");
             infoBox.className = "info-box";
             infoBox.setAttribute("data-type", infoType);
-            infoBox.textContent = displayText;
-    
+
+            // Use innerHTML to insert the HTML content (so the link works)
+            infoBox.innerHTML = displayText;
+
             // Insert info box right after the corresponding button
             let button = document.querySelector(`button[data-type="${infoType}"]`);
             if (button) {
@@ -82,7 +132,6 @@ visaLocationSelect.addEventListener("change", function () {
             }
         }
     };
-    
 
     // Function to display athlete questions based on age
     window.showQuestions = function (ageGroup) {
@@ -95,9 +144,10 @@ visaLocationSelect.addEventListener("change", function () {
         let kcse = document.querySelector('input[name="kcse"]:checked')?.value;
         let eu = document.querySelector('input[name="eu"]:checked')?.value;
         let denial = document.querySelector('input[name="denial"]:checked')?.value;
+        let startdate = document.querySelector('input[name="startdate"]:checked')?.value;
 
         if (english === "yes" && kcse === "yes" && eu === "yes" && denial === "no") {
-            suggestionDiv.textContent = "The athlete can go anywhwere. The first choice in Europe would be Prague, Lisbon, or Brussels. Oslo and Stockholm would work as well, even though a bit more expensive for travelling. Bern is also expensice. Bratislava or Zagreb could work as well.";
+            suggestionDiv.textContent = "The athlete can go anywhere. The first choice in Europe would be Prague, Lisbon, or Brussels. Oslo and Stockholm would work as well, even though a bit more expensive for travelling. Bern is also expensive. Bratislava or Zagreb could work as well.";
         } else {
             suggestionDiv.textContent = "Will be updated.";
         }
