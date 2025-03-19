872fd471a44b9976f04d25648ecab732a92bbeac
nairatch <nairatch@ymail.com>
2025-03-19 22:01:11 +0400

    Visa locations Info updated

---
+++ locationData.js
@@ -0,0 +1,79 @@
+// Export the data so it can be imported in another file
+export { locationData };
+
+const locationData = {
+  "Austria/Vienna":
+    "<br/>We have no prior experience processing third-country national applications in Vienna.  Appointments in Vienna are generally difficult to secure, particularly for non-residents. However, we can explore this option in the future if needed.",
+  "Bahrain/Manama":"<br/>We successfully processed two athlete approvals in Manama during July-August 2024. Both athletes possessed Bahraini nationality. Notably, one athlete, Musa, also held Nigerian citizenship. This experience demonstrates Manama's viability as an approval location. It's important to note, however, that Nigerian citizens require an eVisa for Bahrain, which incurs additional costs. Despite this, if an athlete is already present in Bahrain for a race or similar event, applying in Manama is a beneficial option.",
+  "Belgium/Brussels":
+    "<br/>Our experience with three athlete approvals in 2024-2025 was very positive. All were first-time applicants, and the interview process was manageable, leading to quick approvals. Please be aware that travel expenses are substantial. For local transportation, Uber is a viable option.",
+  "Bosnia & Herzegovina/Sarajevo": "<br/>A Turkish athlete interviewed in Sarajevo in December 2024 experienced a negative outcome, receiving a denial. The athlete declined a subsequent interview in Sarajevo. This single experience has raised concerns regarding approval rates in East European countries, which appear to be comparatively lower than those in Western Europe. We are currently reviewing our approach to applications in this region.",
+  "Croatia/Zagreb":
+    "<br/>We have a 50% approval rate in Zagreb (August 2024), with one approval and one denial. Neither applicant had previous denials. The interview questions were of moderate difficulty. Travel to Zagreb presented some inconsistencies. Therefore, we need to gather additional experience to make a conclusive assessment of this location. Based on our current observations, there is an indication that denial rates may be higher in Central and Southeast European countries, though more data is needed for a definitive conclusion.",
+  "Cyprus/Nicosia": "<br/>We have no prior experience with applications in Cyprus, though it remains a location of interest. We considered sending a Kenyan athlete there, but logistical challenges arose. Direct flights to Nicosia are unavailable, requiring travel through the Turkish Republic of Northern Cyprus. This would involve time, expense, and the complexity of crossing between the two regions. Additionally, Kenyan citizens require both a Schengen visa for Cyprus and a Turkish eTA for transit.",
+  "Czech Republic/Prague":
+    "<br/>Good experience but caution is needed with this location. 4 athletes were approved there. They were of mixed types: first timers, with previous denials, or older ones. Interviews were moderate. The first appointment was fine, then it was followed by a denial with Caren; Afterwards there was an administrative processing for Silas&Cosmas, which was ended with an approval within hours.",
+  "Denmark/Copenhagen": "<br/>Info will be added.",
+  "Estonia/Tallinn":
+    "<br/>Like Vilnius there is no chance for approval, it apparently depends on an interviewer because questions were also difficult. 2 out of 2 were denied, Brenda and Nelly, who had also been denied in Vilnius 2 days before. They got a letter that both countries belong to the same booking system, this turned out to be the reason for this second denial. No further attempts were done in this location.",
+  "Eswatini/Mbabane":
+    "<br/>This location is actually burnt for us! It went good for a while, then messed up. The lady in Eswatini doesn't like to approve of people that were out of school for too long. So, an athlete must have a recent KCSE to go there.",
+  "Finland/Helsinki": "<br/>Info will be added.",
+  "France/Paris": "<br/>Info will be added.",
+  "Germany/Berlin": "<br/>Info will be added.",
+  "Germany/Munich": "<br/>Info will be added.",
+  "Germany/Frankfurt": "<br/>Info will be added.",
+  "Greece/Athens": "<br/>Info will be added.",
+  "Hungary/Budapest": "<br/>Info will be added.",
+  "Iceland/Reykjavik": "<br/>Info will be added.",
+  "India/Chennai": "<br/>Info will be added.",
+  "India/New Delhi": "<br/>Info will be added.",
+  "India/Kolkata": "<br/>Info will be added.",
+  "India/Mumbai": "<br/>Info will be added.",
+  "India/Hyderabad": "<br/>Info will be added.",
+  "Italy/Rome": "<br/>Info will be added.",
+  "Italy/Milan": "<br/>Info will be added.",
+  "Japan/Tokyo":
+    "<br/>Dezmond Chepkosir, a Keynan athlete was approved in 2024. The interview questions were easy. It was actually an exception to apply in this location because the athlete was already in Japan.",
+  "Latvia/Riga": "<br/>Info will be added.",
+
+  "Liberia/Monrovia":
+    "<br/>The appointment system likely has a flag or record for individuals who have previously been denied a visa or entry. When Blessing Ogundiruan's appointment was booked, it was done in a way that didn't properly reflect her prior denial.As a result, when she arrived for her appointment, the system or staff recognized the discrepancy: the system showed she should not have been able to make that type of appointment due to her previous denial. Therefore, they refused to see her.We booked another appointment for her, it happened in early January 2025. <br/> So, pay attention to the fact that previous visa denial history should be correctly reflected in the embassy account.",
+
+  "Lithuania/Vilnius":
+    "<br/>No chance for approval, it apparently depends on an interviewer because questions were also difficult. 2 out of 2 were denied but those 2 were Brenda and Nelly, who also had previous denials. No further attempts were done in this location.",
+  "Luxembourg/Luxembourg": "<br/>Info will be added.",
+  "Madagascar/Antananarivo": "<br/>Info will be added.",
+  "Mauritius/Port Louis":
+    "<br/>It depends on interviewer. No consistent luck with this location but it can still be evaluated. It's very important that no more that one person should go there next time. In 2024 we sent 4 athletes on the same day there and all of them were denied on. No Uber or Bolt is available. Shuttle must be booked upfront or athletes need enough money to pay local for transportation.",
+  "Nigeria/Lagos": "<br/>Info will be added.",
+  "Nigeria/Abuja": "<br/>Info will be added.",
+  "Norway/Oslo":
+    "<br/>2 approvals in Oslo and both were first-try, they had no previous denials. Interviews were easy, the processing time quick (1 day) but the trip expensive.",
+  "Poland/Warsaw":
+    "<br/>No good location for athletes. Interview questions were easy but with no approval, 2 out 2 were denied. There is no chance for an approval because it apparently depends on a visa interviewer unlike Krakow.",
+  "Poland/Krakow":
+    "<br/>A top visa location in Europe! Athletes get approved even after multiple denials. So, it's a good backup option for those who get denied in Europe. We have to be careful with the location though, not many athletes should be sent there. Only those who have no chance elsewhere in Europe or those who are really really important. The trip to Poland is cheap and the hostel has a good locaiton as well.",
+  "Portugal/Lisbon":
+    "<br/>A very positive experience with this location! Interviews were easy and approvals quick. It seems to work well for older athletes with older KCSEs, with tough cases and multiple denials previously. Besides, the trip to Lisbon is not expensive.",
+  "Romania/Bucharest": "<br/>Info will be added.",
+  "Senegal/Dakar": "<br/>Info will be added.",
+  "Serbia/Belgrade": "<br/>Info will be added.",
+  "Slovakia/Bratislava":
+    "<br/>Approval rate is 50/50 and the interiews difficult (Caren denied and Dismus approved). Besides, there are no good flights to Bratislava, one must fly to Vienna and then travel to Bratislava by train.",
+  "South Africa/Cape Town":
+    "<br/>This is the best visa location so far in Africa. It can work as a backup option for all. The consular officer is a nice interviewer, no one was denied, even athletes with multiple previous denials, older or young, with old KCSE or not. Tough cases were resovled and approved in Cape Town. P.S. There is no hostel close to the embassy, Uber is needed.",
+  "South Africa/Durban": "<br/>Info will be added.",
+  "South Africa/Johannesburg":
+    "<br/>This location seems to depend on an interviewer, we avoid Johannesburg for Kenyans, they ideally should go to Cape Town. For other nationalities Johannesburg might be an option though.",
+  "Sri Lanka/Colombo": "<br/>Info will be added.",
+  "Sweden/Stockholm":
+    "<br/>Good experience with the location, the trip was expensive though. There was one athlete with a first try and got approved. The interview was easy.",
+  "Switzerland/Bern":
+    "<br/>50/50. It worked well with the first 2 athletes but the following 2 were denied. None of the athletes had previous denials, the interviews were moderate, not easy but not difficult either. It is generally a good location but caution is needed not to burn it out. Besides, the trip was complicated and expensive (flight to Zurich, train to Bern , etc). Note also that during the most recent trip (Elsingi, Cynthia) the border control called Christian to check regarding entry status/race competitions.",
+  "The Netherlands/Amsterdam": "<br/> Info will be updated",
+  "Togo/Lome": "<br/>Info will be added",
+  "Zambia/Lusaka":
+    "<br/>So far we have had no experience there. It has been used by many other Kenyans. There are 2 consulars, one of which approves and another denies. Apparently a matter of luck.",
+  "Zimbabwe/Harare": "<br/>Info will be added.",
+};
--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+import { locationData } from "./locationData.js";
+import additionalInfoData from "./additionalInfo.js";
+
 document.addEventListener("DOMContentLoaded", function () {
   const introText = document.getElementById("introText");
   const questionnaireDiv = document.getElementById("questionnaire");
@@ -92,7 +95,9 @@ document.addEventListener("DOMContentLoaded", function () {
     let kcse = document.querySelector('input[name="kcse"]:checked')?.value;
     let eu = document.querySelector('input[name="eu"]:checked')?.value;
     let denial = document.querySelector('input[name="denial"]:checked')?.value;
-    let startdate = document.querySelector('input[name="startdate"]:checked')?.value;
+    let startdate = document.querySelector(
+      'input[name="startdate"]:checked'
+    )?.value;
 
     // Ensure the div remains hidden unless a suggestion is made
     if (english && kcse && eu && denial && startdate) {
@@ -168,134 +173,6 @@ document.addEventListener("DOMContentLoaded", function () {
   const infoText = document.getElementById("infoText");
   const additionalInfoDiv = document.getElementById("additionalInfo");
   const additionalInfoText = document.getElementById("additionalInfoText");
-
-  const locationData = {
-    "Austria/Vienna": "Info will be added.",
-    "Bahrain/Manama": "Info will be added.",
-    "Belgium/Brussels":
-      "Good experience with 3x approvals. All athletes were first-timers, interviews were easy but the trip expensive, Uber can be used.",
-    "Bosnia & Herzegovina/Sarajevo": "Info will be added.",
-    "Croatia/Zagreb":
-      "Approval rate is 50/50 with one denial and one approval. Questions were of medium difficulty. The travel to Zagreb was okay but not reliable, more experience is needed therefore.",
-    "Cyprus/Nicosia": "Info will be added.",
-    "Czech Republic/Prague":
-      "Good experience but caution is needed with this location. 4 athletes were approved there. They were of mixed types: first timers, with previous denials, or older ones. Interviews were moderate. The first appointment was fine, then it was followed by a denial with Caren; Afterwards there was an administrative processing for Silas&Cosmas, which was ended with an approval within hours.",
-    "Denmark/Copenhagen": "Info will be added.",
-    "Estonia/Tallinn":
-      "Like Vilnius there is no chance for approval, it apparently depends on an interviewer because questions were also difficult. 2 out of 2 were denied, Brenda and Nelly, who had also been denied in Vilnius 2 days before. They got a letter that both countries belong to the same booking system, this turned out to be the reason for this second denial. No further attempts were done in this location.",
-    "Eswatini/Mbabane":
-      "This location is actually burnt for us! It went good for a while, then messed up. The lady in Eswatini doesn't like to approve of people that were out of school for too long. So, an athlete must have a recent KCSE to go there.",
-    "Finland/Helsinki": "Info will be added.",
-    "France/Paris": "Info will be added.",
-    "Germany/Berlin": "Info will be added.",
-    "Germany/Munich": "Info will be added.",
-    "Germany/Frankfurt": "Info will be added.",
-    "Greece/Athens": "Info will be added.",
-    "Hungary/Budapest": "Info will be added.",
-    "Iceland/Reykjavik": "Info will be added.",
-    "India/Chennai": "Info will be added.",
-    "India/New Delhi": "Info will be added.",
-    "India/Kolkata": "Info will be added.",
-    "India/Mumbai": "Info will be added.",
-    "India/Hyderabad": "Info will be added.",
-    "Italy/Rome": "Info will be added.",
-    "Italy/Milan": "Info will be added.",
-    "Japan/Tokyo":
-      "Dezmond Chepkosir, a Keynan athlete was approves in 2024. The interview questions were easy. It was actually an exception to apply in this location because the athlete was already in Japan.",
-    "Latvia/Riga": "Info will be added.",
-    "Liberia/Monrovia": "Info will be added.",
-    "Lithuania/Vilnius":
-      "No chance for approval, it apparently depends on an interviewer because questions were also difficult. 2 out of 2 were denied but those 2 were Brenda and Nelly, who also had previous denials. No further attempts were done in this location.",
-    "Luxembourg/Luxembourg": "Info will be added.",
-    "Madagascar/Antananarivo": "Info will be added.",
-    "Mauritius/Port Louis":
-      "It depends on interviewer. No consistent luck with this location but it can still be evaluated. It's very important that no more that one person should go there next time. In 2024 we sent 4 athletes on the same day there and all of them were denied on. No Uber or Bolt is available. Shuttle must be booked upfront or athletes need enough money to pay local for transportation.",
-    "Nigeria/Lagos": "Info will be added.",
-    "Nigeria/Abuja": "Info will be added.",
-    "Norway/Oslo":
-      "2 approvals in Oslo and both were first-try, they had no previous denials. Interviews were easy, the processing time quick (1 day) but the trip expensive.",
-    "Poland/Warsaw":
-      "No good location for athletes. Interview questions were easy but with no approval, 2 out 2 were denied. There is no chance for an approval because it apparently depends on a visa interviewer unlike Krakow.",
-    "Poland/Krakow":
-      "A top visa location in Europe! Athletes get approved even after multiple denials. So, it's a good backup option for those who get denied in Europe. We have to be careful with the location though, not many athletes should be sent there. Only those who have no chance elsewhere in Europe or those who are really really important. The trip to Poland is cheap and the hostel has a good locaiton as well.",
-    "Portugal/Lisbon":
-      "A very positive experience with this location! Interviews were easy and approvals quick. It seems to work well for older athletes with older KCSEs, with tough cases and multiple denials previously. Besides, the trip to Lisbon is not expensive.",
-    "Romania/Bucharest": "Info will be added.",
-    "Senegal/Dakar": "Info will be added.",
-    "Serbia/Belgrade": "Info will be added.",
-    "Slovakia/Bratislava":
-      "Approval rate is 50/50 and the interiews difficult (Caren denied and Dismus approved). Besides, there are no good flights to Bratislava, one must fly to Vienna and then travel to Bratislava by train.",
-    "South Africa/Cape Town":
-      "This is the best visa location so far in Africa. It can work as a backup option for all. The consular officer is a nice interviewer, no one was denied, even athletes with multiple previous denials, older or young, with old KCSE or not. Tough cases were resovled and approved in Cape Town. P.S. There is no hostel close to the embassy, Uber is needed.",
-    "South Africa/Durban": "Info will be added.",
-    "South Africa/Johannesburg":
-      "This location seems to depend on an interviewer, we avoid Johannesburg for Kenyans, they ideally should go to Cape Town. For other nationalities Johannesburg might be an option though.",
-    "Sri Lanka/Colombo": "Info will be added.",
-    "Sweden/Stockholm":
-      "Good experience with the location, the trip was expensive though. There was one athlete with a first try and got approved. The interview was easy.",
-    "Switzerland/Bern":
-      "50/50. It worked well with the first 2 athletes but the following 2 were denied. None of the athletes had previous denials, the interviews were moderate, not easy but not difficult either. It is generally a good location but caution is needed not to burn it out. Besides, the trip was complicated and expensive (flight to Zurich, train to Bern , etc). Note also that during the most recent trip (Elsingi, Cynthia) the border control called Christian to check regarding entry status/race competitions.",
-    "The Netherlands/Amsterdam": "Info will be added.",
-    "Togo/Lome": "Info will be added",
-    "Zambia/Lusaka":
-      "So far we have had no experience there. It has been used by many other Kenyans. There are 2 consulars, one of which approves and another denies. Apparently a matter of luck.",
-    "Zimbabwe/Harare": "Info will be added.",
-  };
-
-  const additionalInfoData = {
-    "South Africa/Cape Town": {
-      waitingtime: "30 days (18.02.2025)",
-      approval: "100%",
-      processing: "1 week (3-4 business days for processing + delivery)",
-      approved:
-        "Nelly Jemeli, Brenda Kibor, Mercy Lomuria, Millicent Kemboi, Viola Jepleting, Naomi Jepleting, Faith Kipmaiyo, Caren Kiplagat",
-      denied: "No one denied",
-      questions: [
-        {
-          name: "Viola",
-          questionsAndAnswers: [
-            {
-              question: "Why do you want to go to America?",
-              answer:
-                "I want to go to America to study, and I have been offered a full scholarship which will cover all my study-related costs.",
-            },
-            {
-              question: "How did you get to know about the school?",
-              answer:
-                "I got to know about the school due to my outstanding performance in a time trial, where I ran a good time. The school offered me a full scholarship.",
-            },
-            {
-              question: "Do you live in Mauritius?",
-              answer:
-                "No, I am here for my visa interview because there is no valid visa date in my home country, Kenya.",
-            },
-            {
-              question: "Which school are you going to?",
-              answer: "I'm going to Mississippi State University.",
-            },
-          ],
-        },
-        {
-          name: "Nelly Jemeli",
-          questionsAndAnswers: [
-            {
-              question: "Do you know the coach of the school?",
-              answer: "Yes, I know the coach.",
-            },
-            {
-              question: "What are you going to do in America?",
-              answer: "I will be studying and training in my field.",
-            },
-            {
-              question: "Do you know the major?",
-              answer: "Yes, I will be majoring in Sports Management.",
-            },
-          ],
-        },
-      ],
-    },
-    // Add other locations' additional information here
-  };
 
   function showLocationInfo() {
     const selectedLocation = select.value;
